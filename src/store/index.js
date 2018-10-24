2b8b5bf34a63f97061f3b37b76c279442addf4bd
전솔 <jeonsol12@naver.com>
2018-10-24 16:15:35 +0900

    test

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import firebaseConfig from '@/config/firebase'
+import { firebaseMutations, firebaseAction } from 'vuexfire'
+
+Vue.use(Vuex)
+
+const state = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const getters = {
+
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations
+})
